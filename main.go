86d4df8573f914c2f27f98f2f23679941a574a39
RouSage <rousage@protonmail.com>
2026-01-25 16:31:02 +0100

    Read in a goroutine with a channel

--- main.go
+++ main.go
@@ -14,30 +14,46 @@ func main() {
 	if err != nil {
 		log.Fatal()
 	}
-	defer f.Close()
 
-	txt := make([]byte, 8)
-	currLine := ""
+	for line := range getLinesChannel(f) {
+		fmt.Printf("read: %s\n", line)
+	}
 
-	for {
-		n, err := f.Read(txt)
-		if n > 0 {
-			parts := strings.Split(string(txt), "\n")
-			if len(parts) == 1 {
-				currLine = currLine + parts[0]
-			} else if len(parts) > 1 {
-				currLine = currLine + parts[0]
-				fmt.Printf("read: %s\n", currLine)
-				currLine = strings.Join(parts[1:], "")
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+
+	go func() {
+		defer close(lines)
+		defer f.Close()
+
+		txt := make([]byte, 8)
+		currLine := ""
+
+		for {
+			n, err := f.Read(txt)
+			if n > 0 {
+				parts := strings.Split(string(txt), "\n")
+				if len(parts) == 1 {
+					currLine = currLine + parts[0]
+				} else if len(parts) > 1 {
+					currLine = currLine + parts[0]
+					lines <- currLine
+					currLine = strings.Join(parts[1:], "")
+				}
+			}
+
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					if len(currLine) > 0 {
+						lines <- currLine
+					}
+				}
+				break
 			}
 		}
+	}()
 
-		if errors.Is(err, io.EOF) {
-			break
-		}
-	}
-
-	if currLine != "" {
-		fmt.Printf("read: %s\n", currLine)
-	}
+	return lines
 }
