38d0c1bdd82bad656591aa3b8c3e593d66a71805
RouSage <rousage@protonmail.com>
2026-01-25 16:15:21 +0100

    Read by line

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -16,15 +17,27 @@ func main() {
 	defer f.Close()
 
 	txt := make([]byte, 8)
+	currLine := ""
 
 	for {
 		n, err := f.Read(txt)
 		if n > 0 {
-			fmt.Printf("read: %s\n", txt)
+			parts := strings.Split(string(txt), "\n")
+			if len(parts) == 1 {
+				currLine = currLine + parts[0]
+			} else if len(parts) > 1 {
+				currLine = currLine + parts[0]
+				fmt.Printf("read: %s\n", currLine)
+				currLine = strings.Join(parts[1:], "")
+			}
 		}
 
 		if errors.Is(err, io.EOF) {
 			break
 		}
+	}
+
+	if currLine != "" {
+		fmt.Printf("read: %s\n", currLine)
 	}
 }
