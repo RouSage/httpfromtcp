6b0ec90c6b02080c0621826b16120388c5b24699
RouSage <rousage@protonmail.com>
2026-02-02 17:08:17 +0100

    Parse headers

---
+++ internal/headers/headers.go
@@ -0,0 +1,73 @@
+package headers
+
+import (
+	"errors"
+	"strings"
+)
+
+const crlf = "\r\n"
+
+type Headers map[string]string
+
+func NewHeaders() Headers {
+	return Headers{}
+}
+
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	headersStr := string(data)
+	// if \r\n is not in the string, it needs more data
+	if !strings.Contains(headersStr, crlf) {
+		return 0, false, nil
+	}
+	// if CRLF is at the start of the data, it's the end of headers
+	// return done=true
+	if strings.HasPrefix(headersStr, crlf) {
+		return 0, true, nil
+	}
+
+	headerStr := strings.Split(headersStr, crlf)[0]
+	if headerStr == "" {
+		return 0, false, errors.New("empty header")
+	}
+
+	parts := strings.SplitN(headerStr, ":", 2)
+	if len(parts) != 2 {
+		return 0, false, errors.New("invalid header")
+	}
+
+	parsedKey, ok := parseKey(parts[0])
+	if !ok {
+		return 0, false, errors.New("invalid header key")
+	}
+	parsedValue, ok := parseValue(parts[1])
+	if !ok {
+		return 0, false, errors.New("invalid header value")
+	}
+
+	h[parsedKey] = parsedValue
+
+	return len(headerStr) + len(crlf), false, nil
+}
+
+func parseKey(key string) (string, bool) {
+	// There cannot be an empty space between the key and the colon
+	if strings.HasSuffix(key, " ") {
+		return "", false
+	}
+
+	trimmedKey := strings.TrimSpace(key)
+	if trimmedKey == "" {
+		return "", false
+	}
+
+	return trimmedKey, true
+}
+
+func parseValue(value string) (string, bool) {
+	trimmedValue := strings.TrimSpace(value)
+	if trimmedValue == "" {
+		return "", false
+	}
+
+	return trimmedValue, true
+}
---
+++ internal/headers/headers_test.go
@@ -0,0 +1,64 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHeaders(t *testing.T) {
+	// Test: Valid single header
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+	// Test: Valid single header with extra whitespace
+	headers = NewHeaders()
+	data = []byte("Host:        localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 37, n)
+	assert.False(t, done)
+
+	// Test: Valid 2 headers with existing headers
+	data = []byte("Content-Type: application/json\r\nHeader: value\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "application/json", headers["Content-Type"])
+	assert.Equal(t, 32, n)
+	assert.False(t, done)
+	n, done, err = headers.Parse(data[32:])
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "application/json", headers["Content-Type"])
+	assert.Equal(t, "value", headers["Header"])
+	assert.Equal(t, 15, n)
+	assert.False(t, done)
+	n, done, err = headers.Parse(data[47:])
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "application/json", headers["Content-Type"])
+	assert.Equal(t, "value", headers["Header"])
+	assert.Equal(t, 0, n)
+	assert.True(t, done)
+
+	// Test: Invalid spacing header
+	headers = NewHeaders()
+	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+}
