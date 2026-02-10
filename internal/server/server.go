d635f79df47c0fbe702bbe011f69407e46fbf0a6
RouSage <rousage@protonmail.com>
2026-02-10 16:09:09 +0100

    Basic HTTP server

---
+++ internal/server/server.go
@@ -0,0 +1,57 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync/atomic"
+)
+
+type Server struct {
+	listener net.Listener
+	closed   atomic.Bool
+}
+
+func Serve(port int) (*Server, error) {
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, err
+	}
+
+	s := &Server{
+		listener: l,
+	}
+
+	go s.listen()
+
+	return s, nil
+}
+
+func (s *Server) Close() error {
+	s.closed.Store(true)
+	return s.listener.Close()
+}
+
+func (s *Server) listen() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			if s.closed.Load() {
+				return
+			}
+			log.Fatal(err)
+		}
+
+		go s.handle(conn)
+	}
+}
+
+func (s *Server) handle(conn net.Conn) {
+	defer conn.Close()
+
+	response := []byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello World!\n")
+	_, err := conn.Write(response)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
